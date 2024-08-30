95847a00b31d9d58540788ea41e8a3347c699df8
Fabio Squizzato <fsquizzato@visualmedica.com>
2024-08-30 15:36:46 -0300

    Saldo evoulicon

--- src/components/main.js
+++ src/components/main.js
@@ -40,16 +40,16 @@ export default function Main() {
   const [cantidadPreciosUsados, setCantidadPreciosUsados] = useState([]);
   const [fechasMes, setFechasMes] = useState([]);
   const [balancesMes, setBalancesMes] = useState([]);
-  const [longitudLineChart,setLongitudLineChart] = useState(0);
-  const [fechaMes2,setFechaMes2] = useState([]);
+  const [longitudLineChart, setLongitudLineChart] = useState(0);
+  const [fechaMes2, setFechaMes2] = useState([]);
   const [balancesMes2, setBalancesMes2] = useState([]);
-  const [longitudLineChart2,setLongitudLineChart2] = useState(0);
-  const [dataMes,setDataMes] = useState({});
-  const [mesProvisorio,setMesProvisorio] = useState([]);
-  const [mesProvisorioTotales,setMesProvisorioTotales] = useState([]);
-  const [viajeTotales,setViajesTotales] = useState(0);
-  const [cargaMesTotales,setCargaMesTotales] = useState(0);
-  const [serviciosTotalesMes,setServiciosTotalesMes] = useState(0);
+  const [longitudLineChart2, setLongitudLineChart2] = useState(0);
+  const [dataMes, setDataMes] = useState({});
+  const [mesProvisorio, setMesProvisorio] = useState([]);
+  const [mesProvisorioTotales, setMesProvisorioTotales] = useState([]);
+  const [viajeTotales, setViajesTotales] = useState(0);
+  const [cargaMesTotales, setCargaMesTotales] = useState(0);
+  const [serviciosTotalesMes, setServiciosTotalesMes] = useState(0);
   const [allMesData, setAllMesData] = useState({
     nroMovimientos: 0,
     nroServicios: 0,
@@ -58,9 +58,9 @@ export default function Main() {
     saldoConsumido: 0,
     avgViaje: 0,
     arrServiciosXMes: {},
-    objPrecios: {}
-  })
-
+    objPrecios: {},
+    objEvolucionSaldo: {},
+  });
 
   const handleChange = (event) => {
     setMes(event.target.value);
@@ -72,10 +72,12 @@ export default function Main() {
     setCargaMes(0);
     setDineroCargado(0);
     setSaldoConsumido(0);
-    setMesProvisorioTotales(Uno.Data.Items.filter((item)=>{
-      const fecha = new Date(item.Date);
-      return fecha.getMonth() + 1 === mes || mes ==='all'
-    }))
+    setMesProvisorioTotales(
+      Uno.Data.Items.filter((item) => {
+        const fecha = new Date(item.Date);
+        return fecha.getMonth() + 1 === mes || mes === "all";
+      })
+    );
 
     Uno.Data.Items.forEach((item) => {
       totalMovimientosMes++;
@@ -88,7 +90,7 @@ export default function Main() {
               prev + Number(item.BalanceFormat.slice(2).replace(",", "."))
           );
         }
-        if(item.Type === 'Carga virtual'){
+        if (item.Type === "Carga virtual") {
           setCargaMes((prev) => prev + 1);
           setDineroCargado(
             (prev) =>
@@ -97,10 +99,10 @@ export default function Main() {
         }
       }
     });
-    setMesProvisorio(arrayProvisorio)
+    setMesProvisorio(arrayProvisorio);
     setTotalViajesMes(arrayProvisorio.length);
     setAvgViaje(saldoConsumido / totalViajesMes);
-    setLongitudLineChart(totalViajesMes*10);
+    setLongitudLineChart(totalViajesMes * 10);
     let arrayServicios = [];
     Uno.Data.EntityList.forEach((linea) => {
       let contador = 0;
@@ -140,73 +142,99 @@ export default function Main() {
     });
     setDataPreciosUsados(arrayPreciosUsados);
     setCantidadPreciosUsados(arrayCantidadPreciosUsados);
-    setLongitudLineChart2(0)
+    setLongitudLineChart2(0);
     let arrayBalances = [];
     let arrayFechas = [];
     arrayProvisorio.forEach((item) => {
       arrayBalances.push(item.ValueFormat.slice(2).replace(",", "."));
       arrayFechas.push(new Date(item.Date));
-      setFechaMes2([...fechaMes2,item.Date])
-      setBalancesMes2([...balancesMes2,item.ValueFormat.slice(2).replace(",", ".")])
-      setLongitudLineChart2(prev => prev + 10)
+      setFechaMes2([...fechaMes2, item.Date]);
+      setBalancesMes2([
+        ...balancesMes2,
+        item.ValueFormat.slice(2).replace(",", "."),
+      ]);
+      setLongitudLineChart2((prev) => prev + 10);
     });
     arrayBalances = arrayBalances.reverse();
     arrayFechas = arrayFechas.reverse();
     setFechasMes(arrayFechas);
     setBalancesMes(arrayBalances);
-  },[mes]);
+  }, [mes]);
 
-  useEffect(()=>{
-
-    function saldoSumadora(prev,item){
-      const value = item.Type !== 'Carga virtual' ? Math.abs(Number(item.BalanceFormat.slice(2).replace(",", "."))) : 0;
-      return  prev + value
+  useEffect(() => {
+    function saldoSumadora(prev, item) {
+      const value =
+        item.Type !== "Carga virtual"
+          ? Math.abs(Number(item.BalanceFormat.slice(2).replace(",", ".")))
+          : 0;
+      return prev + value;
     }
-    function saldoCargadora(prev,item){
-      const value = item.Type === 'Carga virtual' ? Math.abs(Number(item.BalanceFormat.slice(2).replace(",", "."))) : 0;
-      return  prev + value
+    function saldoCargadora(prev, item) {
+      const value =
+        item.Type === "Carga virtual"
+          ? Math.abs(Number(item.BalanceFormat.slice(2).replace(",", ".")))
+          : 0;
+      return prev + value;
     }
-    const nroCargas = mesProvisorioTotales.filter(item => item.Type === 'Carga virtual').length;
-    const nroMovimientos =  mesProvisorioTotales.length;
+    const nroCargas = mesProvisorioTotales.filter(
+      (item) => item.Type === "Carga virtual"
+    ).length;
+    const nroMovimientos = mesProvisorioTotales.length;
     const nroServicios = nroMovimientos - nroCargas;
-    const saldoConsumido = mesProvisorioTotales.reduce(saldoSumadora,0)
-    const avgViaje = saldoConsumido/nroServicios
-    const saldoCargado = mesProvisorioTotales.reduce(saldoCargadora,0)
+    const saldoConsumido = mesProvisorioTotales.reduce(saldoSumadora, 0);
+    const avgViaje = saldoConsumido / nroServicios;
+    const saldoCargado = mesProvisorioTotales.reduce(saldoCargadora, 0);
 
-    const arrServiciosXMes = mesProvisorioTotales.reduce((acc,item)=>{
-      if(item.Type !== 'Carga virtual'){
-        const found = acc.find(el=>el.id === item.Entity);
-        if(found){
+    const arrServiciosXMes = mesProvisorioTotales.reduce((acc, item) => {
+      if (item.Type !== "Carga virtual") {
+        const found = acc.find((el) => el.id === item.Entity);
+        if (found) {
           found.value++;
-        }else{
-          acc.push({id:item.Entity,label:item.Entity,value:1})
+        } else {
+          acc.push({ id: item.Entity, label: item.Entity, value: 1 });
         }
-  
       }
       return acc;
-    },[])
+    }, []);
 
     const preciosArray = [];
     const cantidadPreciosArray = [];
 
-    mesProvisorioTotales.reduce((acc,item)=>{
-      if(item.Type !== 'Carga virtual'){
-        const found = acc.find(el=>el.precio === item.BalanceFormat);
-        if(found){
-          found.total++
-        }else{
-          acc.push({precio:item.BalanceFormat,total:1})
+    mesProvisorioTotales
+      .reduce((acc, item) => {
+        if (item.Type !== "Carga virtual") {
+          const found = acc.find((el) => el.precio === item.BalanceFormat);
+          if (found) {
+            found.total++;
+          } else {
+            acc.push({ precio: item.BalanceFormat, total: 1 });
+          }
         }
-      }
-      return acc;
-    },[]).forEach(item => {
-      preciosArray.push(item.precio);
-      cantidadPreciosArray.push(item.total);
+        return acc;
+      }, [])
+      .forEach((item) => {
+        preciosArray.push(item.precio);
+        cantidadPreciosArray.push(item.total);
+      });
+
+    const evolucionFechasSaldo = [];
+    const evolucionSaldos = [];
+
+    mesProvisorioTotales.forEach((item) => {
+      evolucionFechasSaldo.push(new Date(item.Date));
+      evolucionSaldos.push(Number(item.ValueFormat.slice(2).replace(",", ".")));
     });
 
- 
+    /*arrayProvisorio.forEach((item) => {
+      arrayBalances.push(item.ValueFormat.slice(2).replace(",", "."));
+      arrayFechas.push(new Date(item.Date));
+      setFechaMes2([...fechaMes2,item.Date])
+      setBalancesMes2([...balancesMes2,item.ValueFormat.slice(2).replace(",", ".")])
+      setLongitudLineChart2(prev => prev + 10)
+    });*/
 
-    setAllMesData({...allMesData,
+    setAllMesData({
+      ...allMesData,
       nroMovimientos,
       nroCargas,
       nroServicios,
@@ -214,15 +242,11 @@ export default function Main() {
       avgViaje,
       saldoCargado,
       arrServiciosXMes,
-      objPrecios: {preciosArray,cantidadPreciosArray}
+      objPrecios: { preciosArray, cantidadPreciosArray },
+      objEvolucionSaldo: { evolucionFechasSaldo, evolucionSaldos },
+    });
 
-      
-    })
-
-    console.log({allMesData})
-  },[mesProvisorioTotales])
-
-
+  }, [mesProvisorioTotales]);
 
   const inputSelect = (
     <FormControl sx={{ m: 1, minWidth: 200 }}>
@@ -309,42 +333,63 @@ export default function Main() {
     <>
       {inputSelect}
       {arrayDataMes}
-      <p>Movimientos totales mes: {allMesData['nroMovimientos']}</p>
-      <p>Servicios totales mes: {allMesData['nroServicios']}</p>
-      <p>Cargas totales mes: {allMesData['nroCargas']}</p>
-      <p>Saldo Cargado: $ {allMesData['saldoCargado'].toFixed(2)}</p>
-      <p>Saldo Consumido: $ {allMesData['saldoConsumido'].toFixed(2)}</p>
-      <p>Promedio por viaje: $ {allMesData['avgViaje'].toFixed(2)}</p>
+      <p>Movimientos totales mes: {allMesData["nroMovimientos"]}</p>
+      <p>Servicios totales mes: {allMesData["nroServicios"]}</p>
+      <p>Cargas totales mes: {allMesData["nroCargas"]}</p>
+      <p>Saldo Cargado: $ {allMesData["saldoCargado"].toFixed(2)}</p>
+      <p>Saldo Consumido: $ {allMesData["saldoConsumido"].toFixed(2)}</p>
+      <p>Promedio por viaje: $ {allMesData["avgViaje"].toFixed(2)}</p>
 
       <h2>Servicios</h2>
-      {allMesData['arrServiciosXMes'].length && <PieChart
-        slotProps={{
-          legend: {
-            direction: "row",
-            position: { vertical: "bottom", horizontal: "right" },
-            padding: 0,
-          },
-        }}
-        series={[
-          {
-            data: allMesData['arrServiciosXMes'],
-            highlightScope: { faded: "global", highlighted: "item" },
-            faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
-            innerRadius: 30,
-          },
-        ]}
-        width={500}
-        height={300}
-      />}
+      {allMesData["arrServiciosXMes"].length && (
+        <PieChart
+          slotProps={{
+            legend: {
+              direction: "row",
+              position: { vertical: "bottom", horizontal: "right" },
+              padding: 0,
+            },
+          }}
+          series={[
+            {
+              data: allMesData["arrServiciosXMes"],
+              highlightScope: { faded: "global", highlighted: "item" },
+              faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+              innerRadius: 30,
+            },
+          ]}
+          width={500}
+          height={300}
+        />
+      )}
 
-    <h2>Por precio del pasaje</h2>
-      {Object.keys(allMesData.objPrecios).length !== 0 && <BarChart
-        yAxis={[{ scaleType: "band", data: allMesData.objPrecios.preciosArray }]}
-        series={[{ data: allMesData.objPrecios.cantidadPreciosArray }]}
-        width={500}
-        height={300}
-        layout="horizontal"
-      />}
+      <h2>Por precio del pasaje</h2>
+      {Object.keys(allMesData.objPrecios).length !== 0 && (
+        <BarChart
+          yAxis={[
+            { scaleType: "band", data: allMesData.objPrecios.preciosArray },
+          ]}
+          series={[{ data: allMesData.objPrecios.cantidadPreciosArray }]}
+          width={500}
+          height={300}
+          layout="horizontal"
+        />
+      )}
+
+      {Object.keys(allMesData.objEvolucionSaldo).length !== 0 &&<div className="lineChart">
+        <h2>Evolución del saldo</h2>
+        <LineChart
+          grid={{ vertical: true, horizontal: true }}
+          xAxis={[{ data: allMesData.objEvolucionSaldo.evolucionFechasSaldo}]}
+          series={[
+            {
+              data: allMesData.objEvolucionSaldo.evolucionSaldos,
+            },
+          ]}
+          width={1500}
+          height={300}
+        />
+      </div>}
 
       <div className="lineChart">
         <h2>Evolución del saldo</h2>
@@ -360,7 +405,6 @@ export default function Main() {
           height={300}
         />
       </div>
-
     </>
   );
 }
