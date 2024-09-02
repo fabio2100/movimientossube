21a35186b90ff0bea3e04e9d7603de914344de42
fabio2100 <squizzato.fabio@gmail.com>
2024-09-01 21:14:55 -0300

    limpieza codigo

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,10 @@
-import { BarChart, LineChart, PieChart, SparkLineChart } from "@mui/x-charts";
+import { BarChart, PieChart } from "@mui/x-charts";
 import Uno from "../movimientos/total082024.json";
 import {
-  Box,
   FormControl,
   InputLabel,
   MenuItem,
   Select,
-  Stack,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 //Estructura
@@ -29,27 +27,7 @@ import { useEffect, useState } from "react";
 
 export default function Main() {
   const [mes, setMes] = useState("all");
-  const [arrayDataMes, setArrayDataMes] = useState([]);
-  const [arrayServiciosMes, setArrayServiciosMes] = useState([]);
-  const [totalViajesMes, setTotalViajesMes] = useState(0);
-  const [cargaMes, setCargaMes] = useState(0);
-  const [dineroCargado, setDineroCargado] = useState(0);
-  const [saldoConsumido, setSaldoConsumido] = useState(0);
-  const [avgViaje, setAvgViaje] = useState(0);
-  const [dataPreciosUsados, setDataPreciosUsados] = useState([]);
-  const [cantidadPreciosUsados, setCantidadPreciosUsados] = useState([]);
-  const [fechasMes, setFechasMes] = useState([]);
-  const [balancesMes, setBalancesMes] = useState([]);
-  const [longitudLineChart, setLongitudLineChart] = useState(0);
-  const [fechaMes2, setFechaMes2] = useState([]);
-  const [balancesMes2, setBalancesMes2] = useState([]);
-  const [longitudLineChart2, setLongitudLineChart2] = useState(0);
-  const [dataMes, setDataMes] = useState({});
-  const [mesProvisorio, setMesProvisorio] = useState([]);
   const [mesProvisorioTotales, setMesProvisorioTotales] = useState([]);
-  const [viajeTotales, setViajesTotales] = useState(0);
-  const [cargaMesTotales, setCargaMesTotales] = useState(0);
-  const [serviciosTotalesMes, setServiciosTotalesMes] = useState(0);
   const [allMesData, setAllMesData] = useState({
     nroMovimientos: 0,
     nroServicios: 0,
@@ -67,11 +45,6 @@ export default function Main() {
   };
 
   useEffect(() => {
-    let arrayProvisorio = [];
-    let totalMovimientosMes = 0;
-    setCargaMes(0);
-    setDineroCargado(0);
-    setSaldoConsumido(0);
     setMesProvisorioTotales(
       Uno.Data.Items.filter((item) => {
         const fecha = new Date(item.Date);
@@ -79,86 +52,6 @@ export default function Main() {
       })
     );
 
-    Uno.Data.Items.forEach((item) => {
-      totalMovimientosMes++;
-      const fecha = new Date(item.Date);
-      if (fecha.getMonth() + 1 == mes || mes === "all") {
-        if (item.Type !== "Carga virtual") {
-          arrayProvisorio.push(item);
-          setSaldoConsumido(
-            (prev) =>
-              prev + Number(item.BalanceFormat.slice(2).replace(",", "."))
-          );
-        }
-        if (item.Type === "Carga virtual") {
-          setCargaMes((prev) => prev + 1);
-          setDineroCargado(
-            (prev) =>
-              prev + Number(item.BalanceFormat.slice(2).replace(",", "."))
-          );
-        }
-      }
-    });
-    setMesProvisorio(arrayProvisorio);
-    setTotalViajesMes(arrayProvisorio.length);
-    setAvgViaje(saldoConsumido / totalViajesMes);
-    setLongitudLineChart(totalViajesMes * 10);
-    let arrayServicios = [];
-    Uno.Data.EntityList.forEach((linea) => {
-      let contador = 0;
-      arrayProvisorio.forEach((movimiento) => {
-        if (movimiento.Entity == linea) {
-          contador++;
-        }
-      });
-
-      contador > 0 &&
-        arrayServicios.push({ id: linea, label: linea, value: contador });
-    });
-    setArrayServiciosMes(arrayServicios);
-    let arrayTipoMes = [];
-    Uno.Data.MovementTypeList.forEach((tipo) => {
-      arrayTipoMes.push(tipo);
-      arrayProvisorio.forEach((viajeMes) => {
-        if (viajeMes.Type === tipo) {
-        }
-      });
-    });
-
-    let arrayPreciosUsados = [];
-    let arrayCantidadPreciosUsados = [];
-    arrayProvisorio.forEach((element) => {
-      if (arrayPreciosUsados.indexOf(element.BalanceFormat) == -1) {
-        arrayPreciosUsados.push(element.BalanceFormat);
-        arrayCantidadPreciosUsados.push(1);
-      } else {
-        arrayCantidadPreciosUsados[
-          arrayPreciosUsados.indexOf(element.BalanceFormat)
-        ] =
-          arrayCantidadPreciosUsados[
-            arrayPreciosUsados.indexOf(element.BalanceFormat)
-          ] + 1;
-      }
-    });
-    setDataPreciosUsados(arrayPreciosUsados);
-    setCantidadPreciosUsados(arrayCantidadPreciosUsados);
-    setLongitudLineChart2(0);
-    let arrayBalances = [];
-    let arrayFechas = [];
-    arrayProvisorio.forEach((item) => {
-      arrayBalances.push(item.ValueFormat.slice(2).replace(",", "."));
-      arrayFechas.push(new Date(item.Date));
-      setFechaMes2([...fechaMes2, item.Date]);
-      setBalancesMes2([
-        ...balancesMes2,
-        item.ValueFormat.slice(2).replace(",", "."),
-      ]);
-      setLongitudLineChart2((prev) => prev + 10);
-    });
-    arrayBalances = arrayBalances.reverse();
-    arrayFechas = arrayFechas.reverse();
-    setFechasMes(arrayFechas);
-    setBalancesMes(arrayBalances);
   }, [mes]);
 
   useEffect(() => {
@@ -275,64 +168,11 @@ export default function Main() {
     </FormControl>
   );
 
-  let arrayServicios = [];
-  const lineasUsadas = Uno.Data.EntityList.map((linea) => {
-    let contador = 0;
-    Uno.Data.Items.forEach((movimiento) => {
-      movimiento.Entity == linea && contador++;
-    });
-    arrayServicios.push({ id: linea, label: linea, value: contador });
-    return (
-      <p>
-        {linea} Veces:{contador}
-      </p>
-    );
-  });
 
-  let arrayTipos = [];
-  let arrayTiposBarChart = [];
 
-  const tipoDeMovimiento = Uno.Data.MovementTypeList.map(
-    (tipoMovimiento, index) => {
-      let contador = 0;
-      Uno.Data.Items.forEach((movimiento) => {
-        tipoMovimiento == movimiento.Type && contador++;
-      });
-      arrayTipos.push({
-        id: tipoMovimiento,
-        label: tipoMovimiento,
-        value: contador,
-      });
-      arrayTiposBarChart.push(contador);
-      return (
-        <p>
-          {tipoMovimiento} : {contador}
-        </p>
-      );
-    }
-  );
-
-  let arrayBalances = [];
-  let arrayFechas = [];
-  Uno.Data.Items.forEach((item) => {
-    arrayBalances.push(item.ValueFormat.slice(2).replace(",", "."));
-    arrayFechas.push(new Date(item.Date));
-  });
-  arrayBalances = arrayBalances.reverse();
-  arrayFechas = arrayFechas.reverse();
-
-  var options = {
-    month: "short",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    seconds: "numeric",
-  };
-  const valueFormatter = (fecha) => fecha.toLocaleString("es-ES", options);
   return (
     <>
       {inputSelect}
-      {arrayDataMes}
       <p>Movimientos totales mes: {allMesData["nroMovimientos"]}</p>
       <p>Servicios totales mes: {allMesData["nroServicios"]}</p>
       <p>Cargas totales mes: {allMesData["nroCargas"]}</p>
@@ -376,35 +216,6 @@ export default function Main() {
         />
       )}
 
-      {Object.keys(allMesData.objEvolucionSaldo).length !== 0 &&<div className="lineChart">
-        <h2>Evolución del saldo</h2>
-        <LineChart
-          grid={{ vertical: true, horizontal: true }}
-          xAxis={[{ data: allMesData.objEvolucionSaldo.evolucionFechasSaldo}]}
-          series={[
-            {
-              data: allMesData.objEvolucionSaldo.evolucionSaldos,
-            },
-          ]}
-          width={1500}
-          height={300}
-        />
-      </div>}
-
-      <div className="lineChart">
-        <h2>Evolución del saldo</h2>
-        <LineChart
-          grid={{ vertical: true, horizontal: true }}
-          xAxis={[{ data: fechasMes, valueFormatter }]}
-          series={[
-            {
-              data: balancesMes,
-            },
-          ]}
-          width={1500}
-          height={300}
-        />
-      </div>
     </>
   );
 }
